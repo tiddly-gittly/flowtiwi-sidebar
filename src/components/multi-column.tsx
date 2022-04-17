cd69c1970ea043da6345549916890d40d78a72ee
林东吴 <lindongwu@bytedance.com>
2022-04-17 17:43:09 +0800

    feat: add scrollbar

--- src/components/multi-column.tsx
+++ src/components/multi-column.tsx
@@ -60,7 +60,8 @@ export function MultiColumn(props: IMultiColumnProps): JSX.Element {
             }}
             layouts={allLayouts}
             breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}>
+            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+            autoSize>
             {gridChildren}
           </Responsive>
         ) : (
