25efaf973c58e97e5b99816718f1d426ea967abd
林东吴 <lindongwu@bytedance.com>
2022-04-17 10:34:33 +0800

    feat: store state in tiddler

--- src/components/multi-column.tsx
+++ src/components/multi-column.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useFilter } from 'tw-react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
+import { useDebouncedCallback } from 'beautiful-react-hooks';
 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
@@ -9,16 +10,25 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export interface IMultiColumnProps {
   layouts: ReactGridLayout.Layouts;
+  onChange: (newLayouts: ReactGridLayout.Layouts) => any;
 }
 export function MultiColumn(props: IMultiColumnProps): JSX.Element {
+  const [currentBreakpoint, setCurrentBreakpoint] = useState('lg');
+  const [allLayouts, setAllLayouts] = useState(props.layouts);
+  const debouncedOnChange = useDebouncedCallback(props.onChange, [], 1000);
+  const onLayoutChange = (layout: ReactGridLayout.Layout[]) => {
+    const newAllLayouts = { ...allLayouts, [currentBreakpoint]: layout };
+    setAllLayouts(newAllLayouts);
+    debouncedOnChange(newAllLayouts);
+  };
   return (
     <ResponsiveGridLayout
       className="layout"
-      onLayoutChange={(layouts) => {
-        // DEBUG: console
-        console.log(`layouts`, layouts);
+      onLayoutChange={onLayoutChange}
+      onBreakpointChange={(breakpoint, _newCols) => {
+        setCurrentBreakpoint(breakpoint);
       }}
-      layouts={props.layouts}
+      layouts={allLayouts}
       breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
       cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}>
       <div key="1">1</div>
--- src/flowtiwi-sidebar.ts
+++ src/flowtiwi-sidebar.ts
@@ -8,18 +8,11 @@ class FlowTiWiWidget extends Widget<IMultiColumnProps> {
   reactComponent = MultiColumn;
   getProps = () => {
     const stateTiddler = this.getAttribute('stateTiddler', '$:/plugins/linonetwo/flowtiwi-sidebar/state');
-    const layoutsRaw = $tw.wiki.getTiddlerText(stateTiddler) ?? '[]';
-    let layouts: ReactGridLayout.Layouts = {};
-    try {
-      layouts = JSON.parse(layoutsRaw) as ReactGridLayout.Layouts;
-    } catch (error) {
-      console.error(
-        `$:/plugins/linonetwo/flowtiwi-sidebar Error: JSON.parse(layoutsRaw) failed, layoutsRaw is ${layoutsRaw} ${(error as Error).message} ${
-          (error as Error).stack
-        }`,
-      );
+    const layouts: ReactGridLayout.Layouts = $tw.wiki.getTiddlerData(stateTiddler, {})
+    const onChange = (newLayouts: ReactGridLayout.Layouts) => {
+      $tw.wiki.setTiddlerData(stateTiddler, newLayouts);
     }
-    return { layouts };
+    return { layouts, onChange };
   };
 }
 
