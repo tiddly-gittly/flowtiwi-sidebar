0f4700eedfb48bd988fdbbd4aa9c9e6ad0b23112
lin onetwo <linonetwo012@gmail.com>
2022-04-19 22:35:25 +0800

    fix: importPageMacros to render list widget

--- src/components/useRenderTiddler.ts
+++ src/components/useRenderTiddler.ts
@@ -5,10 +5,16 @@ export function useRenderTiddler(tiddlerTitle: string, containerRef: RefObject<H
     if (containerRef.current === null) {
       return;
     }
-    const transcludeWidgetNode = $tw.wiki.makeTranscludeWidget(tiddlerTitle, { document, parentWidget: $tw.rootWidget, recursionMarker: 'no' });
+    const transcludeWidgetNode = $tw.wiki.makeTranscludeWidget(tiddlerTitle, {
+      document,
+      parentWidget: $tw.rootWidget,
+      recursionMarker: 'yes',
+      mode: 'block',
+      importPageMacros: true,
+    });
     const tiddlerContainer = document.createElement('div');
     containerRef.current.append(tiddlerContainer);
     transcludeWidgetNode.render(tiddlerContainer, null);
-    $tw.hooks.invokeHook("th-page-refreshed");
+    $tw.hooks.invokeHook('th-page-refreshed');
   }, [tiddlerTitle, containerRef.current]);
 }
