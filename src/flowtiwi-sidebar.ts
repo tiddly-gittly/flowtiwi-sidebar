84e3f9e198d556ad48c360d7c7ecc0f2d9b92154
lin onetwo <linonetwo012@gmail.com>
2022-04-24 15:04:39 +0800

    feat: provide debounce for refresh, to save cpu

--- src/flowtiwi-sidebar.ts
+++ src/flowtiwi-sidebar.ts
@@ -1,12 +1,24 @@
 import { IChangedTiddlers } from 'tiddlywiki';
 import type { IReactWidget } from 'tw-react';
+import debounce from 'lodash/debounce';
 
 import { IMultiColumnProps, MultiColumn } from './components/multi-column';
 import './style.css';
 
 const Widget = (require('$:/plugins/linonetwo/tw-react/widget.js') as { widget: IReactWidget }).widget;
 
+const DEBOUNCE_UPDATE_INTERVAL = 2000;
+
 class FlowTiWiWidget extends Widget<IMultiColumnProps> {
+  constructor(parseTreeNode: any, options: any) {
+    super(parseTreeNode, options);
+    const debouncedRefresh = debounce(this.refresh.bind(this), DEBOUNCE_UPDATE_INTERVAL);
+    this.refresh = (changedTiddlers: IChangedTiddlers) => {
+      debouncedRefresh(changedTiddlers);
+      return true;
+    };
+  }
+
   refresh(changedTiddlers: IChangedTiddlers) {
     return this.refreshChildren(changedTiddlers);
   }
